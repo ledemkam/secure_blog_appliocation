0c973e1980e13be25b9f5d8d8f048425577b2561
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-03-31 20:42:46 +0200

    feat(dto): add CreatePostRequest and UpdatePostRequest DTOs for post creation and updates

---
+++ src/main/java/com/kte/blog_app/domain/dto/request/UpdatePostRequest.java
@@ -0,0 +1,30 @@
+package com.kte.blog_app.domain.dto.request;
+
+import com.kte.blog_app.domain.entities.PostStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdatePostRequest {
+    @NotNull(message = "Post ID is required")
+    private Long id;
+
+    @NotBlank(message = "Title is required")
+    @Size(min = 3, max = 200, message = "Title must be between {min} and {max} characters")
+    private String title;
+
+    @NotBlank(message = "Content is required")
+    @Size(min = 10, max = 50000, message = "Content must be between {min} and {max} characters")
+    private String content;
+
+    @NotNull(message = "category is required")
+    private PostStatus category;
+}
