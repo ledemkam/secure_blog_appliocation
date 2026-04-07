2b4437fd2f29931d84dc14496a6cd44f469c2da7
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-04-07 05:12:35 +0200

    feat(config): add H2 database configuration and JWT settings

--- src/main/java/com/kte/blog_app/BlogAppApplication.java
+++ src/main/java/com/kte/blog_app/BlogAppApplication.java
@@ -2,8 +2,9 @@ package com.kte.blog_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.webflux.autoconfigure.WebFluxAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = WebFluxAutoConfiguration.class)
 public class BlogAppApplication {
 
 	public static void main(String[] args) {
