a9164e40d1a6dc484e066a1671d17fe881a256d5
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-04-11 23:45:57 +0200

    feat(post): update PostRepository methods to filter by category instead of status

--- src/main/java/com/kte/blog_app/repositories/PostRepository.java
+++ src/main/java/com/kte/blog_app/repositories/PostRepository.java
@@ -2,6 +2,7 @@ package com.kte.blog_app.repositories;
 
 import com.kte.blog_app.domain.entities.Post;
 import com.kte.blog_app.domain.entities.PostStatus;
+import com.kte.blog_app.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +11,7 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    List<Post> findAllByStatus(PostStatus status);
+    List<Post> findAllByCategory(PostStatus category);
 
-    List<Post> findAllByAuthorAndStatus(String author, PostStatus status);
+    List<Post> findAllByAuthorAndCategory(User author, PostStatus status);
 }
