fb65370f5aa5b7bc4ba05d91b15ae77a6275ddf8
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-04-12 22:22:19 +0200

    feat(security): add PostSecurityService for post access control and user validation

--- src/main/java/com/kte/blog_app/services/UserService.java
+++ src/main/java/com/kte/blog_app/services/UserService.java
@@ -2,6 +2,9 @@ package com.kte.blog_app.services;
 
 import com.kte.blog_app.domain.entities.User;
 
+import java.util.Optional;
+
 public interface UserService {
     User getUserId(Long id);
+    Optional<User> findByEmail(String email);
 }
