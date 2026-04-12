52cb1cb89a04f45de74f5583abd4ce06c501c271
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-04-12 22:22:40 +0200

    feat(user): add logging for user retrieval and implement findByEmail method

--- src/main/java/com/kte/blog_app/services/impl/UserServiceImpl.java
+++ src/main/java/com/kte/blog_app/services/impl/UserServiceImpl.java
@@ -5,9 +5,12 @@ import com.kte.blog_app.exceptions.UserNotFoundException;
 import com.kte.blog_app.repositories.UserRepository;
 import com.kte.blog_app.services.UserService;
 import lombok.RequiredArgsConstructor;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -16,7 +19,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUserId(Long id) {
-        return userRepository.findById(id).orElseThrow(() ->new UserNotFoundException
-                ("User not found with  id: " + id));
+        log.debug("Getting user by id: {}", id);
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User not found with id: " + id));
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        log.debug("Finding user by email: {}", email);
+        return userRepository.findByEmail(email);
     }
 }
