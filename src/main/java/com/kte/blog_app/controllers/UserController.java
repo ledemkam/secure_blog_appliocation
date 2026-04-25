e258f1dd1c61230e76cbd3d5f778e64742a1ba8e
Eric k. <81742990+ledemkam@users.noreply.github.com>
2026-04-25 15:22:26 +0200

    feat(controller): add update user endpoint with security checks and request validation

--- src/main/java/com/kte/blog_app/controllers/UserController.java
+++ src/main/java/com/kte/blog_app/controllers/UserController.java
@@ -1,11 +1,15 @@
 package com.kte.blog_app.controllers;
 
 import com.kte.blog_app.controllers.ui_controllers.IUserController;
+import com.kte.blog_app.domain.dto.request.UpdateUserRequest;
 import com.kte.blog_app.domain.entities.User;
+import com.kte.blog_app.security.UserSecurityService;
 import com.kte.blog_app.services.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -17,6 +21,7 @@ import java.util.Optional;
 public class UserController implements IUserController {
 
     private final UserService userService;
+    private final UserSecurityService userSecurityService;
 
     @Override
     @GetMapping("/{id}")
@@ -45,5 +50,17 @@ public class UserController implements IUserController {
             log.debug("No user found with email: {}", email);
             return ResponseEntity.notFound().build();
         }
+    }
+
+
+    @Override
+    @PutMapping("/{id}")
+    @PreAuthorize("@userSecurityService.canUpdateUser(#id)")  // only usewr can update
+    public ResponseEntity<User> updateUser(@PathVariable Long id,
+                                           @Valid @RequestBody UpdateUserRequest updateUserRequest) {
+        log.info("Received request to update user with ID: {}", id);
+        User updatedUser = userService.updateUser(id, updateUserRequest);
+        log.info("Successfully updated user with ID: {}", id);
+        return ResponseEntity.ok(updatedUser);
     }
 }
